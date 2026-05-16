091c6b5df45fd093f755b66a418792787a2602c5
roblencheg <is@volonterski.ru>
2026-05-16 17:52:48 +0700

    ci: add ruff --diff to see suggested fix

--- custom_components/cesar_smart/__init__.py
+++ custom_components/cesar_smart/__init__.py
@@ -1,6 +1,7 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
+
 from .const import DOMAIN, PLATFORMS
 from .coordinator import CesarSmartCoordinator
 
