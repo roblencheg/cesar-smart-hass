7d91d51afb178f288f3e6b7767cbac45b613a9aa
roblencheg <is@volonterski.ru>
2026-05-16 17:46:12 +0700

    Fix hassfest errors: manifest key ordering and CONFIG_SCHEMA

--- custom_components/cesar_smart/__init__.py
+++ custom_components/cesar_smart/__init__.py
@@ -1,8 +1,12 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
 
 from .const import DOMAIN, PLATFORMS
 from .coordinator import CesarSmartCoordinator
+
+
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
